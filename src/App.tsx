3096d9b34a07441c41cba0d2a2675b6c42a5edbc
hiago <hiagohps360@gmail.com>
2021-02-02 14:08:23 -0300

    feat: config root import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '~/components/Header';
 
 function App() {
-  return <div className="App">Youtube</div>;
+  return (
+    <div className="App">
+      <Header />
+      Youtube
+    </div>
+  );
 }
 
 export default App;
